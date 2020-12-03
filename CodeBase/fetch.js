6151471eb71c75b7edfb03ef912b135c7f4d27ce
Ed <lookatemail@gmail.com>
2020-12-03 21:13:23 +0000

    Able to fetch and populate data from json file to DOM

--- CodeBase/fetch.js
+++ CodeBase/fetch.js
@@ -11,4 +11,25 @@ const getText = () => {
     .catch((error) => console.log(error));
 };
 
+const getUsers = () => {
+  fetch('../Data/users.json')
+    .then((res) => {
+      return res.json();
+    })
+    .then((users) => {
+      let userData = '<h3>Users</h3>';
+      users.forEach((user) => {
+        userData += `<ul>
+            <li>ID: ${user.id}</li>
+            <li>Name: ${user.name}</li>
+            <li>Username: ${user.username}</li>
+            <li>Email: ${user.email}</li>
+          </ul>`;
+      });
+      document.getElementById('userData').innerHTML = userData;
+    })
+    .catch((error) => console.log(error));
+};
+
 document.getElementById('getText').addEventListener('click', getText);
+document.getElementById('getUsers').addEventListener('click', getUsers);
